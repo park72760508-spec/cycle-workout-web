eea782d0a16e534f0c331f6b901bb20c07a1aa63
park72760508-spec <park72760508@gmail.com>
2025-10-13 00:02:32 +0900

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -877,7 +877,7 @@ document.addEventListener("DOMContentLoaded", () => {
        const id = card?.getAttribute("data-id");
        const user = list.find((x) => String(x.id) === String(id));
        if (user && typeof window.selectProfile === "function") {
-         window.selectProfile(user); // ← 여기!
+         window.selectProfile(user.id); // ← 여기!
        }
      };
    }
